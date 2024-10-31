9d10ba350a1c304fcd9f957ea95fa9b345665a26
Jdragon <lee411806@naver.com>
2024-10-31 19:21:53 +0900

    init commit

---
+++ src/main/java/com/sparta/settlementplatform/SettlementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.settlementplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlementPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SettlementPlatformApplication.class, args);
+    }
+
+}
